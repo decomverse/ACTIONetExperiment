3a60b9ea31172bf1edb2d43c689a199b73630c78
Shahin Mohammadi <mohammadi@insitro.com>
2021-07-19 06:46:42 -0700

    Updated ace object

--- R/utils_internal.R
+++ R/utils_internal.R
@@ -156,3 +156,20 @@
 
     return(labels)
 }
+
+.tscalet <- function(A, center = TRUE, scale = TRUE){
+  A = Matrix::t(scale(Matrix::t(A), center = center, scale = scale))
+  return(A)
+}
+
+.preprocess_design_matrix_and_var_names <- function(design_mat, variable_name = NULL){
+  if (is.null(variable_name)) {
+    variable_name = colnames(design_mat)[ncol(design_mat)]
+  }
+  vn_idx = which(variable_name == colnames(design_mat))[1]
+  colnames(design_mat) = make.names(colnames(design_mat), unique = TRUE, allow_ = FALSE)
+  variable_name = colnames(design_mat)[vn_idx]
+
+  out = list(design_mat = design_mat, variable_name = variable_name)
+  return(out)
+}
